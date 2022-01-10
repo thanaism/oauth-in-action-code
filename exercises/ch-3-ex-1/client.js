2f4a1398e1851363d8e51ac0b57ca040ec3784e5
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-01-10 10:33:15 +0900

    Implement chapter 3.2.2

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -27,8 +27,8 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
@@ -48,15 +48,46 @@ app.get('/authorize', function(req, res){
 	/*
 	 * Send the user to the authorization server
 	 */
-	
+	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0]
+	});
+
+	console.log("redirect", authorizeUrl);
+	res.redirect(authorizeUrl);
 });
 
 app.get('/callback', function(req, res){
 
 	/*
 	 * Parse the response from the authorization server and get a token
-	 */
+	 */	
+	var code  = req.query.code;
 	
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+
+	var tokRes = request('POST', authServer.tokenEndpoint,
+		{
+			body: form_data,
+			headers: headers
+		}
+	);
+
+	var body = JSON.parse(tokRes.getBody());
+	access_token = body.access_token;
+	console.log('Got access token: %s', access_token);
+
+	res.render('index', { access_token: body.access_token, scope: scope });
+
 });
 
 app.get('/fetch_resource', function(req, res) {
